aa005d29be1557d8e893369d35491d405686e249
Ahosan Habib <150356585+Ahosan310@users.noreply.github.com>
2024-01-05 00:14:27 +0600

    Delete conversion.js

--- conversion.js
+++
@@ -1,8 +0,0 @@
-function radian_to_Degree(radians){
-    return (radians*(180/Math.PI)).toFixed(2);
-    }
-
- let radians = parseFloat(prompt("Enter a temperature(radian): "));
- let degrees = radian_to_Degree(radians);
-
-document.getElementById("demo").innerHTML = "The conversion(radian to degree) is = "+" "+degrees+" "+"degree.";
